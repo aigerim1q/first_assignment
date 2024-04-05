0ff7dea3a260cfa44666e8be3f1e1dd973f4e01e
Toma <230434@astanait.edu.kz>
2024-04-05 13:49:02 +0600

    second task

---
+++ src/task2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class task2 {
+    public static void printaverage(int arr[],int n) {
+        float a=0;
+        for(int i=0;i<n;i++){
+             a=a+arr[i];
+        }
+        float average=a/n;
+        System.out.println(average);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        for (int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        printaverage(arr,n);
+    }
+
+
+}
