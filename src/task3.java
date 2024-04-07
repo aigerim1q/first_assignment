b0bdf192aa53e9dc5db7b053deb1980bc57848b4
Toma <230434@astanait.edu.kz>
2024-04-07 21:27:52 +0600

    third task

---
+++ src/task3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class task3 {
+    public static void printprime(int n) {
+        if (n==2){
+            System.out.println("prime");
+        } else if (n%2!=0) {
+            System.out.println("prime");
+
+        }
+
+        else{
+            System.out.println("composite");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printprime(n);
+    }
+
+
+}
